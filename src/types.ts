773e32c65efc19136add9116fbf8f52d8f9ec6ed
fumito80 <fumito80@gmail.com>
2023-01-26 18:10:13 +0900

    fix v0.21.3

--- src/types.ts
+++ src/types.ts
@@ -125,7 +125,7 @@ export const initialOptions = {
   bmAutoFindTabsDelay: '500',
   restoreSearching: true,
   favColorPalettes: [] as ColorPalette[],
-  showMinimizeAll: false,
+  showMinimizeAll: true,
 };
 
 export type Panes = typeof panes[number];
